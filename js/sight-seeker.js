c2cb0032aaea05139843212983a742b456fd28b9
sean-michael-mlvt <smm65270@uga.edu>
2023-04-24 17:30:33 -0400

    Introduced JavaScript
    Simple, temporary JavaScript before tackling the main features

---
+++ js/sight-seeker.js
@@ -0,0 +1,12 @@
+let score = 0;
+
+const confirm = document.getElementById("confirmBtn");
+const map = document.getElementById("campus");
+
+console.log(campus.height);
+
+confirm.addEventListener("click", function(e) {
+    console.log("Confirm Button Clicked");
+    console.log(e);
+});
+
