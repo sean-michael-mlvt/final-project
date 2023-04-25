dcbf9474acade47ddc405e41d4793dc204aca986
sean-michael-mlvt <smm65270@uga.edu>
2023-04-25 13:48:18 -0400

    Added End Screen
    Added end screen after a few new locations. Disabled confirm button after the locations have been exhausted.

--- js/sight-seeker.js
+++ js/sight-seeker.js
@@ -18,11 +18,14 @@ let textScore = document.getElementById("score");
 const paths = [
     "assets/img/mlc-back.jpg",
     "assets/img/founders-garden.jpg",
-    "assets/img/jfh-commons.jpg"
+    "assets/img/jfh-commons.jpg",
+    "assets/img/driftmier.jpg",
+    "assets/img/bdm-hall.jpg",
+    "assets/img/the-end.jpg"
 ];
 //Percentages of width/height of map
-const xWeights = [0.45, 0.45, 0.76];
-const yWeights = [0.30, 0.20, 0.87];
+const xWeights = [0.45, 0.45, 0.76, 0.44, 0.27];
+const yWeights = [0.30, 0.20, 0.87, 0.86, 0.38];
 
 console.log(map.width);
 console.log(map.height);
@@ -47,30 +50,35 @@ mappin.addEventListener('click', function(e) {
 
 //Submits the Guess when Confirm is Clicked
 confirm.addEventListener("click", function(e) {
-    //Log Guess
-    console.log("Confirm Button Clicked");
-    console.log("Guess: " + guess);
-    //Calculate Correct Answer
-    correctX = map.width * xWeights[index];
-    correctY = map.height * yWeights[index];
-    console.log("Correct (" + correctX + ", " + correctY + ")" );
-
-    //Calculate Distance From Guess to Answer
-    let differenceX = Math.abs(xPos - correctX);
-    let differenceY = Math.abs(yPos - correctY);
-    let c2 = (differenceX * differenceX) + (differenceY * differenceY);
-    let distance = Math.sqrt(c2);
-    console.log("You were " + distance + " away");
-
-    //Adjust and Update Score
-    let adjustment = 0;
-    let max = Math.sqrt(map.width * map.width + map.height * map.height)
-    adjustment = 100 * (1 - (distance/max));
-    score += adjustment;
-    textScore.innerText = "Score: " + Math.round(score);
-
-    //Update Index and Image
-    index++;
-    currentImg.src = paths[index];
+    //Condition: If there are no more locations
+    //In the future, will need to be changed since
+    //I hope to randomize the order
+    if (index < paths.length - 1) {
+        //Log Guess
+        console.log("Confirm Button Clicked");
+        console.log("Guess: " + guess);
+        //Calculate Correct Answer
+        correctX = map.width * xWeights[index];
+        correctY = map.height * yWeights[index];
+        console.log("Correct (" + correctX + ", " + correctY + ")" );
+    
+        //Calculate Distance From Guess to Answer
+        let differenceX = Math.abs(xPos - correctX);
+        let differenceY = Math.abs(yPos - correctY);
+        let c2 = (differenceX * differenceX) + (differenceY * differenceY);
+        let distance = Math.sqrt(c2);
+        console.log("You were " + distance + " away");
+    
+        //Adjust and Update Score
+        let adjustment = 0;
+        let max = Math.sqrt(map.width * map.width + map.height * map.height)
+        adjustment = 100 * (1 - (distance/max));
+        score += adjustment;
+        textScore.innerText = "Score: " + Math.round(score);
+    
+        //Update Index and Image
+        index++;
+        currentImg.src = paths[index];
+    }
 });
 
