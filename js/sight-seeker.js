3a4c8ffbaf8c0ab6304ee3b3ee193c13a2cac8f1
sean-michael-mlvt <smm65270@uga.edu>
2023-04-24 23:02:45 -0400

    Added Photos w/ Locations
    Stored locations and mages in arrays and calculated the correct position by using a ratio of the width/height to account for resizing.
    
    -To-Do:
    -Ensure images get cropped to size well
    -Calculate the distance between guess and correct answer using Pythagorean Theorem with a and b being absolute values of the difference between guess and correct values.

--- js/sight-seeker.js
+++ js/sight-seeker.js
@@ -10,7 +10,18 @@ let pin = document.getElementById("pin");
 const mappin = document.getElementById("mappin");
 const infoText = document.getElementById("txtInfo");
 
-console.log(pin);
+//Correct Answer Information
+const paths = [
+    "assets/img/mlc-back.jpg",
+    "assets/img/founders-garden.jpg",
+    "assets/img/jfh-commons.jpg"
+];
+//Percentages of width/height of map
+const xWeights = [0.45, 0.45, 0.76];
+const yWeights = [0.30, 0.20, 0.87];
+
+console.log(map.width);
+console.log(map.height);
 
 //Drops the Pin when the map div is clicked.
 mappin.addEventListener('click', function(e) {
@@ -30,5 +41,8 @@ mappin.addEventListener('click', function(e) {
 confirm.addEventListener("click", function(e) {
     console.log("Confirm Button Clicked");
     console.log("Guess: " + guess);
+    correctX = map.width * xWeights[0];
+    correctY = map.height * yWeights[0];
+    console.log("Correct (" + correctX + ", " + correctY + ")" );
 });
 
