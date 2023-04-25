0f3867efb32ca07cac1768f09f462d9fdcdf0bc4
sean-michael-mlvt <smm65270@uga.edu>
2023-04-24 20:03:30 -0400

    Mouse Location Tracking
    Added mouse location tracking within the map, actively updating coordinates to the info text.
    
    -Boolean toggle can be made false to halt the process. Use this with click to drop the pin and save location.

--- js/sight-seeker.js
+++ js/sight-seeker.js
@@ -1,9 +1,23 @@
 let score = 0;
+let toggle = true;
 
 const confirm = document.getElementById("confirmBtn");
 const map = document.getElementById("campus");
+const mappin = document.getElementById("mappin");
+const pin = document.getElementById("pin");
+const infoText = document.getElementById("txtInfo");
 
-console.log(campus.height);
+
+
+mappin.addEventListener('mousemove', function(e) {
+    if (toggle) {
+        const target = e.target;
+        const rect = target.getBoundingClientRect();
+        let xPos = e.clientX - rect.left;
+        let yPos = e.clientY - rect.top;
+        infoText.innerText = `(${xPos}, ${yPos})`;
+    } 
+});
 
 confirm.addEventListener("click", function(e) {
     console.log("Confirm Button Clicked");
