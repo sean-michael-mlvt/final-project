46064431712e21b16d8d973766d431a7bd8467cb
sean-michael-mlvt <smm65270@uga.edu>
2023-04-24 22:33:32 -0400

    Drop Pin on Click
    Added JavaScript functionality to place the pin image where the mouse is located relative to the map, accounting for clicking on the pin itself.
    
    Added console.log() commands to track variables as they're updated.
    
    -To-Do:
    -Round pixel values to integer so long as it doesn't affect visual precision.
    -Account for varying sizes of the image (percentages of the map's current width?)

--- js/sight-seeker.js
+++ js/sight-seeker.js
@@ -1,26 +1,34 @@
 let score = 0;
 let toggle = true;
+let xPos = 0;
+let yPos = 0;
+let guess = "";
 
 const confirm = document.getElementById("confirmBtn");
 const map = document.getElementById("campus");
+let pin = document.getElementById("pin");
 const mappin = document.getElementById("mappin");
-const pin = document.getElementById("pin");
 const infoText = document.getElementById("txtInfo");
 
+console.log(pin);
 
-
-mappin.addEventListener('mousemove', function(e) {
-    if (toggle) {
-        const target = e.target;
-        const rect = target.getBoundingClientRect();
-        let xPos = e.clientX - rect.left;
-        let yPos = e.clientY - rect.top;
-        infoText.innerText = `(${xPos}, ${yPos})`;
-    } 
+//Drops the Pin when the map div is clicked.
+mappin.addEventListener('click', function(e) {
+    
+    const target = e.target;
+    const rect = target.getBoundingClientRect();
+    xPos = e.clientX - rect.left;
+    yPos = e.clientY - rect.top;
+    pin.style.top = (yPos - 42) + "px";
+    pin.style.left = (xPos - 14) + "px";
+    infoText.innerText = `(${xPos}, ${yPos})`;
+    guess = "(" + xPos + ", " + yPos + ")";
+    console.log("Pin placed at: " + guess);
 });
 
+//Submits the Guess
 confirm.addEventListener("click", function(e) {
     console.log("Confirm Button Clicked");
-    console.log(e);
+    console.log("Guess: " + guess);
 });
 
