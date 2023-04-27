d9385bf69ad475324cd7c54592ecf110574c50f6
sean-michael-mlvt <smm65270@uga.edu>
2023-04-26 22:23:18 -0400

    Score Calculation v2
    Adjusted Score calculation to make all values from 0-100 possible.
    
    Before: Distance of error was scaled to the maximum possible line on the map (diagonal), but a user could never practically be that far, resulting in a minimum score of 14-30.
    
    After: Distance of error is scaled to the maximum possible error for the given location (location->farthest corner), so a user can actually guess on the lowest end of the scale, resulting in a minimum score of 0-1.

--- js/sight-seeker.js
+++ js/sight-seeker.js
@@ -15,6 +15,7 @@ const currentImg = document.getElementById("current-img");
 let textScore = document.getElementById("score");
 
 //Correct Answer Information
+//Image Source Paths
 const paths = [
     "assets/img/mlc-back.jpg",
     "assets/img/founders-garden.jpg",
@@ -23,7 +24,7 @@ const paths = [
     "assets/img/bdm-hall.jpg",
     "assets/img/the-end.jpg"
 ];
-//Percentages of width/height of map
+//Percentages of width/height of map for locations
 const xWeights = [0.45, 0.45, 0.76, 0.44, 0.27];
 const yWeights = [0.30, 0.20, 0.87, 0.86, 0.38];
 
@@ -65,13 +66,14 @@ confirm.addEventListener("click", function(e) {
         //Calculate Distance From Guess to Answer
         let differenceX = Math.abs(xPos - correctX);
         let differenceY = Math.abs(yPos - correctY);
-        let c2 = (differenceX * differenceX) + (differenceY * differenceY);
-        let distance = Math.sqrt(c2);
+        let distance = pythagorean(differenceX, differenceY);
         console.log("You were " + distance + " away");
     
         //Adjust and Update Score
         let adjustment = 0;
-        let max = Math.sqrt(map.width * map.width + map.height * map.height)
+        //Deprecated Max Calculation:
+        /*let max = Math.sqrt(map.width * map.width + map.height * map.height);*/
+        let max = 5 + getMax();
         adjustment = 100 * (1 - (distance/max));
         score += adjustment;
         textScore.innerText = "Score: " + Math.round(score);
@@ -82,3 +84,36 @@ confirm.addEventListener("click", function(e) {
     }
 });
 
+//Gets Maximum Possible Error in a Guess (px)
+//CX CY = correctX/Y
+function getMax() {
+    const CX = xWeights[index];
+    const CY = yWeights[index];
+    let sideX = 0;
+    let sideY = 0;
+    if (CX >= 0.5 && CY >= 0.5) {
+        //Calc From Top Left
+        sideX = (map.width * CX);
+        sideY = (map.height * CY);
+    } else if (CX < 0.5 && CY>= 0.5) {
+        //Calc From Top Right
+        sideX = (map.width * (1 - CX));
+        sideY = (map.height * CY);
+    } else if (CX < 0.5 && CY < 0.5) {
+        //Calc From Bottom Right
+        sideX = (map.width * (1 - CX));
+        sideY = (map.height * (1 - CY));
+    } else if (CX >= 0.5 && CY < 0.5) {
+        //Calc from Bottom Left
+        sideX = (map.width * CX);
+        sideY = (map.height * (1 - CY));
+    }
+    return pythagorean(sideX, sideY);
+} //getMax
+
+//Finds the diagonal of the right triangle with
+//side lengths a and b
+function pythagorean(a, b) {
+    let cSquared = (a * a) + (b * b);
+    return Math.sqrt(cSquared);
+} //pythagorean
